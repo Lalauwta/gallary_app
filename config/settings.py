567b02ebc007c3526f36ef1b06b1f1d0bd3cec24
Lalauwta <lytinghai168@gmail.com>
2026-06-17 01:12:49 +0700

    Configure PostgreSQL database connection

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -52,8 +53,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'WcGYAAidhbXyjdqZRDcOBGdkbBGBkrBH',
+        'HOST': 'roundhouse.proxy.rlwy.net',
+        'PORT': '35670',
     }
 }
 
