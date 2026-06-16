e9d2d32eed0b505d1bee415057a1d1cfdd95a8cb
Lalauwta <lytinghai168@gmail.com>
2026-06-17 02:36:38 +0700

    Allow serving media in production when SERVE_MEDIA env var is True

--- config/urls.py
+++ config/urls.py
@@ -2,9 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+import os
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('', include('gallery.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve media files during development or when explicitly enabled in production
+serve_media = getattr(settings, 'DEBUG', False) or os.environ.get('SERVE_MEDIA', 'False') == 'True'
+if serve_media:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
