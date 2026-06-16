9719592ee4f28f7960cd24cae3ae55227b97a16c
Lalauwta <lytinghai168@gmail.com>
2026-06-17 02:30:49 +0700

    Allow login by email or username in LoginForm

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,20 +1,46 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(widget=forms.TextInput(attrs={
         'class': 'form-control',
-        'placeholder': 'Enter username',
+        'placeholder': 'Username or email',
     }))
     password = forms.CharField(widget=forms.PasswordInput(attrs={
         'class': 'form-control',
         'placeholder': 'Enter password',
     }))
 
+    def clean(self):
+        # Allow users to login with either username or email
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        if username and '@' in username:
+            try:
+                user = User.objects.get(email__iexact=username)
+                # replace with actual username so AuthenticationForm authenticates correctly
+                cleaned_data['username'] = user.username
+                self.cleaned_data['username'] = user.username
+            except User.DoesNotExist:
+                # leave as-is; AuthenticationForm will raise the invalid login error
+                pass
+        return cleaned_data
+
 
 class RegisterForm(UserCreationForm):
+    username = forms.CharField(
+        max_length=150,
+        validators=[UnicodeUsernameValidator()],
+        widget=forms.TextInput(attrs={
+            'class': 'form-control',
+            'placeholder': 'Enter username',
+            'pattern': '[A-Za-z0-9@.+\-_]{1,150}',
+            'title': '150 chars max. Letters, digits and @ . + - _ only'
+        })
+    )
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={
         'class': 'form-control',
         'placeholder': 'Enter email',
@@ -36,3 +62,8 @@ class RegisterForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs.setdefault('class', 'form-control')
+
+    def clean_username(self):
+        username = self.cleaned_data.get('username', '')
+        # strip surrounding whitespace
+        return username.strip()
